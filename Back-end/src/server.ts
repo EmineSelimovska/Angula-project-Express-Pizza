a8746364cce1bf464e8cc1b58f59eb12495bed95
EmineSelimovska <emi956@abv.bg>
2023-09-01 01:10:18 +0300

    update

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -25,7 +25,7 @@ app.use(cors({
 }));
 app.use(
     cors({
-      origin: "*",
+      origin: "/api/*",
       methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
       credentials: true,
     })
