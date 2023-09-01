fe7d06b373236856ab7038677642db091e372f93
EmineSelimovska <emi956@abv.bg>
2023-09-01 15:39:24 +0300

    update

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -34,9 +34,9 @@ app.use("/api/orders", order);
 
 
 
-app.use(express.static('../../public'));
+app.use(express.static('../../dist/my-project'));
 app.use('/*', (req, res) => {
-    res.sendFile(path.join(__dirname,'../../public', 'index.html'))
+    res.sendFile(path.join(__dirname,'../../dist/my-project', 'index.html'))
 })
 
 
