e4e2cd0ae5c4e975f2edd6c4856cae942819a2e4
EmineSelimovska <emi956@abv.bg>
2023-09-08 22:11:24 +0300

    Update server.ts

--- Back-end/src/server.ts
+++ Back-end/src/server.ts
@@ -40,7 +40,7 @@ app.use(function(req, res, next) {
 
 
 app.use(express.static('../../my-project/dist/my-project'));
-app.use('/api', (req, res) => {
+app.use('/*', (req, res) => {
     res.sendFile(path.join(__dirname,'../../my-project/dist/my-project/index.html'))
 })
 
